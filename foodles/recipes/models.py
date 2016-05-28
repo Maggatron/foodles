4e9cdc41b8c45f4b5c2108c7d8eb3d4f1103d146
Maggatron <m.karanasiou@gmail.com>
2016-05-28 17:59:21 +0100

    made my recipes model

--- foodles/recipes/models.py
+++ foodles/recipes/models.py
@@ -2,4 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200)
+    ingredients = ListField(models.CharField(max_length=50))
+    quantities = ListField(models.CharField(max_length=50))
+    method = models.TextField()
